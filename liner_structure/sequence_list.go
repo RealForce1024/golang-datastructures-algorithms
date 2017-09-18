d42cc052c5c304e6db6ee7c24a00d62bb412cfe6
fqc <337940626@qq.com>
2017-09-18 14:49:02 +0800

    顺序表获取指定位置的元素

--- liner_structure/sequence_list.go
+++ liner_structure/sequence_list.go
@@ -8,13 +8,23 @@ const MAXSIZE = 10 // 存储空间初始分配量
 
 type SeqList struct {
 	// 数组存储数据元素，最大为maxsize。  线性表的最大容量maxsize。存储空间的起始位置就是数组的存储位置。
-	// 线性表的长度小于等于数组的长度
+	// 线性表的长度<=数组的长度
 	data   [MAXSIZE]ElementType
 	length int // 线性表的当前长度
+}
+
+func (list SeqList) getElem(i int) (ElementType, error) {
+	if i < 0 && i > MAXSIZE {
+		panic("参数有误，请传递正确范围0-MaxSize")
+	}
+
+	return list.data[i-1], nil
 }
 
 func main() {
 	var arr = [MAXSIZE]ElementType{1, 2, 3}
 	var sqList = SeqList{data: arr, length: 10}
 	fmt.Println(sqList)
+
+	fmt.Println(sqList.getElem(11))
 }
