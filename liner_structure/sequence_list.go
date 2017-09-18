b9f889cf1dc6ce2d0510523399b53a1eee200c55
fqc <337940626@qq.com>
2017-09-18 15:42:03 +0800

    顺序表中插入元素

--- liner_structure/sequence_list.go
+++ liner_structure/sequence_list.go
@@ -14,22 +14,45 @@ type SeqList struct {
 }
 
 func (list SeqList) getElem(i int) (ElementType, error) {
-	if len(list.data) == 0 {
+	if list.length == 0 {
 		panic("顺序表数组为空,请添加元素后再进行获取指定位置的元素")
 	}
 
-	if i < 1 || i > MAXSIZE {
-		panic("参数有误，请传递正确范围1-MaxSize")
+	if i < 1 || i > list.length {
+		panic("参数有误，请传递正确范围1~length")
 	}
 
 	return list.data[i-1], nil
 }
 
-func main() {
-	// var arr = [MAXSIZE]ElementType{1, 2, 3}
-	var arr = [MAXSIZE]ElementType{}
-	var sqList = SeqList{data: arr, length: 10}
-	fmt.Println(sqList)
+func (list *SeqList) addElem(i int, e ElementType) { //注意传递指针
+	if list.length == MAXSIZE {
+		panic("顺序表已满")
+	}
 
-	fmt.Println(sqList.getElem(11))
+	// if i < 1 || i > MAXSIZE { //注意:实际的长度与容量，添加元素看实际的长度
+	if i < 1 || i > list.length+1 { //
+		panic("要添加的元素不在已有位置的位置内")
+	}
+
+	for k := list.length - 1; k >= i-1; k-- {
+		list.data[k+1] = list.data[k]
+	}
+
+	list.data[i-1] = e
+	list.length++
+}
+func main() {
+	var arr = [MAXSIZE]ElementType{1, 2, 3}
+	// var arr = [MAXSIZE]ElementType{}
+	// var arr [MAXSIZE]ElementType
+	var sqList = SeqList{data: arr}
+
+	// sqList.addElem(2, 33)
+	sqList.addElem(1, 11)
+	sqList.addElem(2, 22)
+	fmt.Println(sqList.length)
+	fmt.Println(sqList)
+	fmt.Println(sqList.getElem(2))
+
 }
