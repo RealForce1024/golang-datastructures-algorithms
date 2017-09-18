6e06e5e70594fe4b29aa85c48abc3d4b9b8ccfd7
fqc <337940626@qq.com>
2017-09-18 14:34:29 +0800

    线性表存储结构的补充说明

--- liner_structure/sequence_list.go
+++ liner_structure/sequence_list.go
@@ -2,13 +2,15 @@ package main
 
 import "fmt"
 
-type ElementType int
+type ElementType int // 元素类型，假设为int
 
-const MAXSIZE = 3
+const MAXSIZE = 10 // 存储空间初始分配量
 
 type SeqList struct {
-	data   [MAXSIZE]ElementType // 线性表的容量 数组存储数据元素，最大为maxsize
-	length int                  // 线性表的当前长度
+	// 数组存储数据元素，最大为maxsize。  线性表的最大容量maxsize。存储空间的起始位置就是数组的存储位置。
+	// 线性表的长度小于等于数组的长度
+	data   [MAXSIZE]ElementType
+	length int // 线性表的当前长度
 }
 
 func main() {
