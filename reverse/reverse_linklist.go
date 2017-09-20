b1e0e2321269759f3749eb5491872a67bcd6f2a7
fqc <337940626@qq.com>
2017-09-20 16:32:12 +0800

    reverse linklist

--- reverse/reverse_linklist.go
+++ reverse/reverse_linklist.go
@@ -1,10 +1,56 @@
 package main
 
-type LinkNode struct {
-	node    *LinkNode
-	content string
+type List struct {
+	value int
+	next  *List
+}
+
+func printList(l *List) {
+	curr := l
+
+	for curr != nil {
+		print(curr.value)
+		if curr.next != nil {
+			print("->")
+		}
+		curr = curr.next
+	}
+}
+
+func reverse(l *List) *List {
+	curr := l
+	var prev *List
+
+	for curr != nil {
+		//记录当前下一节点
+		next := curr.next
+		curr.next = prev
+		if next == nil {
+			break
+		}
+		prev = curr
+		curr = next
+	}
+	return curr
 }
 
 func main() {
+	list := &List{
+		value: 1,
+		next: &List{
+			value: 2,
+			next: &List{
+				value: 3,
+				next: &List{
+					value: 4,
+				},
+			},
+		},
+	}
+
+	printList(list)
+	println()
+
+	printList(reverse(list))
 
 }
